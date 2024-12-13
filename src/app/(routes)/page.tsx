790410722d7535d979c91746a8f13a9bbca93e76
Luis Eduardo <lerivadeneira@pucesi.edu.ec>
2024-12-12 23:31:09 -0500

    billboard implementado

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,11 +1,15 @@
+import { getBillboard } from "@/actions/get-billboard";
+
 import { Billboard } from "@/components/billboard";
 import { Container } from "@/components/ui/container";
 
-const HomePage = () => {
+export const revalidate = 0;
+const HomePage = async () => {
+  const data = await getBillboard("392623b0-d946-4f5e-8eec-7450f8e1b6c7");
   return (
     <Container>
       <div className="space-y-10 mb-10">
-        <Billboard />
+        <Billboard data={data} />
         <div>Home Page</div>
         <div>Home Page</div>
         <div>Home Page</div>
